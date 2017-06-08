8934e62fc505d18b439491e0526ddd641de4b38c
Heggy Castaneda <heggyy@gmail.com>
2017-06-08 00:28:47 -0700

    change todos array

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ console.log('My Todos:', todos);
 // It should have a way to add new todos
 todos.push('item new');
 todos.push('item 5');
+
+// It should have a way to change a todo
+todos[1] = 'I am new' 
+// output: ["item 1", "I am new", "item 3", "item new", "item 5"]
