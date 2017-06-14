3c1541ca78ee62b40c4b33ec5d097dc9d260ce72
Heggy Castaneda <heggyy@gmail.com>
2017-06-14 11:00:42 -0700

    tighten all codes, ( ) if not completed todo vs. (x) .completed is true

--- version5.js
+++ version5.js
@@ -3,20 +3,24 @@
 var todoList = {
     // todos will have objects instead of simple values
     todos: [],
-    displayTodos: function () {
+    displayTodos: function() {
         if (this.todos.length === 0) {
             console.log('Your todo list is empty!');
         } else {
-        // print todos as normal
-            console.log ('My Todos:');
-        // i++ same i = i + 1 
+            console.log('My Todos: ');
             for (var i = 0; i < this.todos.length; i++) {
-            // only todoText property
-                console.log (this.todos[i].todoText);
+                // check if .completed is true
+                if (this.todos[i].completed === true){
+        // completed; print with (x) only todoText property ith element
+                    console.log ('(x)', this.todos[i].todoText);
+                } else {
+        // not completed; print with ( ) only todoText property ith element
+                    console.log('( )', this.todos[i].todoText);
+                }
             }
         }
     },
-    addTodo: function (todoText) { 
+    addTodo: function(todoText) { 
         // add new item inside array of todos
         this.todos.push({
             // key (name of property): value that user passes in
@@ -27,14 +31,14 @@ var todoList = {
         this.displayTodos();
     }, 
     // update parameter newValue > todoText
-    changeTodo: function (position, todoText) {
+    changeTodo: function(position, todoText) {
         // this.todos[position] = todoText;
         // using dot notation to grab just todoText property to update its preexisting value to new todoText value
         var todo = this.todos[position];
         this.todos[position].todoText = todoText;
         this.displayTodos();
     },
-    deleteTodo: function (position) {
+    deleteTodo: function(position) {
         // splice remove one item from index position
         this.todos.splice(position, 1);
         this.displayTodos();
