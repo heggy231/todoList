1c1ead40742d23a431ac49c6d35cb0be2f0bc732
Heggy Castaneda <heggyy@gmail.com>
2017-06-28 16:31:30 -0700

    Big lesson for V7: We are seeing thing more on user interface then console.  Crawling out of console!

--- version7.js
+++ version7.js
@@ -77,10 +77,14 @@ var todoList = {
 // 1. We want to get access to the display todos button.
 // document represents the DOM
 var displayTodosButton = document.getElementById('displayTodosButton');
-console.log(displayTodosButton);
+
+var toggleAllButton = document.getElementById('toggleAllButton');
 // 2. We want to run displayTodos method, when someone clicks
 //    the display todos button.
-
 displayTodosButton.addEventListener('click', function(){
     todoList.displayTodos();
-}); 
+});
+
+toggleAllButton.addEventListener('click', function(){
+    todoList.toggleAll();
+});
