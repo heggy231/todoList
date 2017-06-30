2b419c6d760e9c840a273a12dcec4d9d4a9b6f11
Heggy Castaneda <heggyy@gmail.com>
2017-06-30 16:04:24 -0700

    - Got rid of todoList.displayTodos method - view.displayTodos now does todoList.displayTodos but better since it is displaying on DOM - handlers obj provides all user interactions - view obj only renders data for user to see on the page not on console anymore

--- version9.js
+++ version9.js
@@ -3,24 +3,6 @@
 var todoList = {
   // todos will have objects instead of simple values
   todos: [],
-  displayTodos: function() {
-    // debugger;
-    if (this.todos.length === 0) {
-      console.log('Your todo list is empty!');
-    } else {
-      console.log('My Todos: ');
-      for (var i = 0; i < this.todos.length; i++) {
-        // check if .completed is true
-        if (this.todos[i].completed === true){
-    // completed; print with (x) only todoText property ith element
-          console.log ('(x)', this.todos[i].todoText);
-        } else {
-    // not completed; print with ( ) only todoText property ith element
-          console.log('( )', this.todos[i].todoText);
-        }
-      }
-    }
-  },
   addTodo: function(todoText) {
     // debugger;
     // add new item inside array of todos
@@ -30,7 +12,6 @@ var todoList = {
       todoText: todoText,
       completed: false,
     });
-    this.displayTodos();
   }, 
   // update parameter newValue > todoText
   changeTodo: function(position, todoText) {
@@ -38,19 +19,16 @@ var todoList = {
     // using dot notation to grab just todoText property to update its preexisting value to new todoText value
     var todo = this.todos[position];
     this.todos[position].todoText = todoText;
-    this.displayTodos();
   },
   deleteTodo: function(position) {
     // splice remove one item from index position
     this.todos.splice(position, 1);
-    this.displayTodos();
   },
   toggleCompleted: function(position) {
     // here we use bracket notation since this.todos[position] position is variable
     // if you are refer to the key then use dot notation.
     var todo = this.todos[position];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   toggleAll: function(){
     // debugger;
@@ -75,7 +53,6 @@ var todoList = {
         this.todos[i].completed = true;
       }
     }
-    this.displayTodos();
   }
 };
 
@@ -89,14 +66,12 @@ var todoList = {
 
 
 var handlers = {
-  displayTodos: function(){
-    todoList.displayTodos();
-  },
   addTodo: function(){
     var addTodoTextInput = document.getElementById('addTodoTextInput');
     todoList.addTodo(addTodoTextInput.value);
     // resets addTodo text field
     addTodoTextInput.value = '';
+    view.displayTodos();
   },
   changeTodo: function(){
 // changeTodo: function(position, todoText) note position is number .valueAsNumber
@@ -106,19 +81,23 @@ var handlers = {
 
     changeTodoPositionInput.value = '';
     changeTodoTextInput.value = '';
+    view.displayTodos();
   },
   deleteTodo: function(){
     var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = '';
+    view.displayTodos();
   },
   toggleCompleted: function(){
     var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
     toggleCompletedPositionInput.value = '';
+    view.displayTodos();
   },
   toggleAll: function(){
     todoList.toggleAll();
+    view.displayTodos();
   }
 };
 
